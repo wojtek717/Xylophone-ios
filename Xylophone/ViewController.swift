1f673b681942d60b4cebff964dbfdf6bfa3eb8b5
wojtek717 <wojciechkonury@gmail.com>
2020-02-18 19:39:24 +0100

    play sound for each button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func cKeyPressed(_ sender: UIButton) {
+        playSound(title: sender.titleLabel!.text!)
+    }
+    
+    func playSound(title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
